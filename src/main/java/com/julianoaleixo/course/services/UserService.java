fab6fbb28433c7730037835eca7134aaab6ee25f
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-12-18 17:23:10 -0300

    :sparkles: Feat - Adding service layer and component register

---
+++ src/main/java/com/julianoaleixo/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.julianoaleixo.course.services;
+
+import com.julianoaleixo.course.entities.User;
+import com.julianoaleixo.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
