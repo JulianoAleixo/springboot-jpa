12cf125b6940a56970498455bd3fb45c6900851d
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-12-20 23:20:06 -0300

    :sparkles: Feat - Adding Product

---
+++ src/main/java/com/julianoaleixo/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.julianoaleixo.course.repositories;
+
+import com.julianoaleixo.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
