5ee69d8c641a9cc4aa925555d64c635f48660804
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-12-21 20:24:24 -0300

    :sparkles: Feat - Adding Exception - findById

--- src/main/java/com/julianoaleixo/course/services/UserService.java
+++ src/main/java/com/julianoaleixo/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.julianoaleixo.course.services;
 
 import com.julianoaleixo.course.entities.User;
 import com.julianoaleixo.course.repositories.UserRepository;
+import com.julianoaleixo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
