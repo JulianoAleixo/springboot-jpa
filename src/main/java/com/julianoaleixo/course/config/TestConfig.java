bea6113efa8bf88589ef095b98c3b1dcdfe94d93
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-12-20 23:04:12 -0300

    :sparkles: Feat - Adding Category

--- src/main/java/com/julianoaleixo/course/config/TestConfig.java
+++ src/main/java/com/julianoaleixo/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.julianoaleixo.course.config;
 
+import com.julianoaleixo.course.entities.Category;
 import com.julianoaleixo.course.entities.Order;
 import com.julianoaleixo.course.entities.User;
 import com.julianoaleixo.course.entities.enums.OrderStatus;
+import com.julianoaleixo.course.repositories.CategoryRepository;
 import com.julianoaleixo.course.repositories.OrderRepository;
 import com.julianoaleixo.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999999", "123456");
@@ -35,5 +40,11 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
